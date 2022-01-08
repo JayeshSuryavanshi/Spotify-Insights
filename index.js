375fa05929dbfb9ad9505950b819911b82b03cba
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-01-08 14:23:10 -0500

    minor additions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require ('express');
 const app = express();
 const port = 8000;
+
+//To check if client ID is being displayed on the console
+console.log(process.env.CLIENT_ID);
 
 app.get('/', (req, res) => {
     const data = {
@@ -10,6 +14,11 @@ app.get('/', (req, res) => {
 
     res.json(data);
 });
+
+app.get('/fantastic-generator', (req, res) => {
+    const {name, isFantastic} = req.query;
+    res.send('${name} is ${JSON.parse(isAwesome) ? really} awesome');
+})
 
 app.listen(port, () => {
     console.log('Express app listensing at http://localhost:${port}');
