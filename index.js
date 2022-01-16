2ee0c6bf10459ee2b161e9bda93626790227efdf
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-01-15 20:29:06 -0500

    callback functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require ('express');
 const app = express();
 const port = 8000;
 
+//
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
@@ -30,6 +31,11 @@ app.get('/login', (req, res) => {
     // res.send('Test login to Spotify');
     res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
 });
+
+app.get('/callback', (req, res) => {
+    //callback functionality after the user logs into Spotify account
+    res.redirect()
+})
 
 app.listen(port, () => {
     console.log(`Express app listensing at http://localhost:${port}`);
