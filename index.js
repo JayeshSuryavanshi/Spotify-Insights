ca6847f815ced4016c030e4f98594ec8deb7e120
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-01-17 17:35:39 -0500

    changes in .env, .nvmrc and .gitignore

--- index.js
+++ index.js
@@ -1,50 +1,88 @@
-require('dotenv').config()
-const express = require ('express');
-const querystring = require('querystring');
-const app = express();
-const port = 8000;
 
+require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
+let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8000/callback';
+let FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
+const PORT = process.env.PORT || 8000;
 
+if (process.env.NODE_ENV !== 'production') {
+  REDIRECT_URI = 'http://localhost:8000/callback';
+  FRONTEND_URI = 'http://localhost:3000';
+}
 
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const history = require('connect-history-api-fallback');
 
-//To check if client ID is being displayed on the console
-console.log(process.env.CLIENT_ID);
-
-app.get('/', (req, res) => {
-    const data = {
-        name: 'Jayesh',
-        isFantastic: true
-    };
-
-    res.json(data);
-});
-
-
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
 const generateRandomString = length => {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
-  
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 const stateKey = 'spotify_auth_state';
 
-// app.get('/fantastic-generator', (req, res) => {
-//     const {name, isFantastic} = req.query;
-//     res.send('${name} is ${JSON.parse(isAwesome) ? really} awesome');
-// })
+// Multi-process to utilize all CPU cores.
+if (cluster.isMaster) {
+  console.warn(`Node cluster master ${process.pid} is running`);
 
-app.get('/login', function (req, res) {
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`,
+    );
+  });
+} else {
+  const app = express();
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app
+    .use(express.static(path.resolve(__dirname, '../client/build')))
+    .use(cors())
+    .use(cookieParser())
+    .use(
+      history({
+        verbose: true,
+        rewrites: [
+          { from: /\/login/, to: '/login' },
+          { from: /\/callback/, to: '/callback' },
+          { from: /\/refresh_token/, to: '/refresh_token' },
+        ],
+      }),
+    )
+    .use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app.get('/', function (req, res) {
+    res.render(path.resolve(__dirname, '../client/build/index.html'));
+  });
+
+  app.get('/login', function (req, res) {
     const state = generateRandomString(16);
     res.cookie(stateKey, state);
 
-    //application requests authorization
+    // your application requests authorization
     const scope =
       'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
 
@@ -59,11 +97,83 @@ app.get('/login', function (req, res) {
     );
   });
 
-app.get('/callback', (req, res) => {
-    //callback functionality after the user logs into Spotify account
-    res.redirect()
-})
+  app.get('/callback', function (req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
 
-app.listen(port, () => {
-    console.log(`Express app listensing at http://localhost:${port}`);
-});
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+      res.redirect(`/#${querystring.stringify({ error: 'state_mismatch' })}`);
+    } else {
+      res.clearCookie(stateKey);
+      const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: REDIRECT_URI,
+          grant_type: 'authorization_code',
+        },
+        headers: {
+          Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+            'base64',
+          )}`,
+        },
+        json: true,
+      };
+
+      request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const access_token = body.access_token;
+          const refresh_token = body.refresh_token;
+
+          // we can also pass the token to the browser to make requests from there
+          res.redirect(
+            `${FRONTEND_URI}/#${querystring.stringify({
+              access_token,
+              refresh_token,
+            })}`,
+          );
+        } else {
+          res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+        }
+      });
+    }
+  });
+
+  app.get('/refresh_token', function (req, res) {
+    // requesting access token from refresh token
+    const refresh_token = req.query.refresh_token;
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+          'base64',
+        )}`,
+      },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token,
+      },
+      json: true,
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const access_token = body.access_token;
+        res.send({ access_token });
+      }
+    });
+  });
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.warn(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  });
+}
