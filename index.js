78991f27f40ea40991d31cf281abc58a740c45ed
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2022-01-17 17:21:02 -0500

    QueryParams addition

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require ('express');
+const querystring = require('querystring');
 const app = express();
 const port = 8000;
 
-//
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
@@ -28,6 +29,12 @@ app.get('/', (req, res) => {
 // })
 
 app.get('/login', (req, res) => {
+
+    const queryParams = querystring.stringify({
+        client_id : CLIENT_ID,
+        response_type : 'code',
+        redirect_uri: REDIRECT_URI,
+    })
     // res.send('Test login to Spotify');
     res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
 });
